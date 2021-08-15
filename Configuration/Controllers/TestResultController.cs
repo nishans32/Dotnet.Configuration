b53b2701f1e19ec290a58f3f8b98fb43f0ee9557
Nishan Siriwardena <nishan.siriwardena@xero.com>
2021-08-15 19:28:30 +1000

    Return the config values from the controller

---
+++ Configuration/Controllers/TestResultController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Confuguration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestResultController : ControllerBase
+    {
+        public ClientSecret _clientSecret { get; }
+
+
+        public TestResultController(IOptions<ClientSecret> clientSecret)
+        {
+            _clientSecret = clientSecret.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(JsonSerializer.Serialize(_clientSecret));
+        }
+    }
+}
